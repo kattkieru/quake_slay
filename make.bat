24bdb97bddda37c07d394d7be9637ad82e42e1d5
Charles Wardlaw <charles@skeletalstudios.com>
2019-11-26 00:35:52 -0500

    Not installing any more.

--- make.bat
+++ make.bat
@@ -5,5 +5,5 @@ mkdir build
 pushd build
 cmake -G "Visual Studio 15 2017 Win64" -DVULKAN_SDK:STRING=D:/vulkan_sdk/1.1.126.0 ..
 popd
-cmake --build build --config Release --target install
+cmake --build build --config Release
 
