4dca6771b4f19073cf30323fec6649dfa5370e1b
Charles Wardlaw <charles@skeletalstudios.com>
2019-11-26 00:21:24 -0500

    Continuing build process.

--- make.bat
+++ make.bat
@@ -3,7 +3,7 @@ git submodule update --init --recursive
 REM git submodule foreach --recursive git fetch
 mkdir build
 pushd build
-cmake -G "Visual Studio 15 2017 Win64" ..
+cmake -G "Visual Studio 15 2017 Win64" -DVULKAN_SDK:STRING=D:/vulkan_sdk/1.1.126.0 ..
 popd
 cmake --build build --config Release --target install
 
